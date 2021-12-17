55621a57aeacdf92cfb8229cbc5f21e349aae357
Pedrinho-o-brabo <preitman@itelinfo.com>
2021-12-17 14:39:02 -0400

    add andar repo

---
+++ src/main/java/com/vespertino/hotelvesp/repositories/AndarRepository.java
@@ -0,0 +1,14 @@
+package com.vespertino.hotelvesp.repositories;
+
+import com.vespertino.hotelvesp.entities.Andar;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+
+public interface AndarRepository extends JpaRepository<Andar,Integer> {
+    List<Andar> findByAtivo(Boolean ativo);
+
+}git
