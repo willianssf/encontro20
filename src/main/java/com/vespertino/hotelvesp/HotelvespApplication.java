f824236b2a061d6a35fd0119e9482f4e0a517d9b
Pedrinho-o-brabo <preitman@itelinfo.com>
2021-12-17 14:11:54 -0400

    teste

--- src/main/java/com/vespertino/hotelvesp/HotelvespApplication.java
+++ src/main/java/com/vespertino/hotelvesp/HotelvespApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HotelvespApplication {
 
+
 	public static void main(String[] args) {
+		System.out.println("DemoApp...");
+
 		SpringApplication.run(HotelvespApplication.class, args);
+		System.out.println("DemoApp inicializado");
 	}
 
 }
