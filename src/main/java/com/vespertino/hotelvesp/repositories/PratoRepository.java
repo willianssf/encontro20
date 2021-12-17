cf9715bfc1d41505a061d3d79a5a7f02b723d12c
Willianssf <wfranca@itelinfo.com>
2021-12-17 14:46:40 -0400

    PratoRepository

---
+++ src/main/java/com/vespertino/hotelvesp/repositories/PratoRepository.java
@@ -0,0 +1,13 @@
+package com.vespertino.hotelvesp.repositories;
+
+import com.vespertino.hotelvesp.entities.Prato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PratoRepository extends JpaRepository<Prato, Integer> {
+
+    List<Prato> findByAtivo(Boolean ativo);
+}
