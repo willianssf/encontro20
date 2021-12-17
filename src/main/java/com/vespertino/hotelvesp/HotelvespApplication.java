c8467edaa0b4a92e6d44049dc0ec2c65b03c82c1
Willian <wfranca@itelinfo.com>
2021-12-17 13:59:31 -0400

    projeto criado

---
+++ src/main/java/com/vespertino/hotelvesp/HotelvespApplication.java
@@ -0,0 +1,13 @@
+package com.vespertino.hotelvesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelvespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelvespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vespertino/hotelvesp/HotelvespApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vespertino.hotelvesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelvespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
