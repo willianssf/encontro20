024f9c1150eea5ff84fec90581ca081a44b74fea
ViniciusPaulaSouza <vsouza@itelinfo.com>
2021-12-17 15:12:29 -0400

    Repositorio PratoPedido

---
+++ src/main/java/com/vespertino/hotelvesp/repositories/PratoPedidoRepository.java
@@ -0,0 +1,12 @@
+package com.vespertino.hotelvesp.repositories;
+
+import com.vespertino.hotelvesp.entities.Pedido;
+import com.vespertino.hotelvesp.entities.PratoPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PratoPedidoRepository extends JpaRepository<PratoPedido,Integer> {
+    List<PratoPedido> findByAtivo(Boolean ativo);
+
+}
