2e58c0d3863afb9b64862a8d84d915422093ab39
Pedrinho-o-brabo <preitman@itelinfo.com>
2021-12-17 14:14:50 -0400

    add mesg

---
+++ src/main/java/com/vespertino/hotelvesp/Mensagem.java
@@ -0,0 +1,13 @@
+package com.vespertino.hotelvesp;
+
+public class Mensagem {
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    private String mensagem;
+}
