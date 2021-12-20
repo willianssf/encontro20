e8a4447f490ce23a8a56246e6dfa4898b677bf3e
enzookita <eokita@itelinfo.com>
2021-12-20 16:37:24 -0400

    oooo

--- src/main/java/com/vespertino/hotelvesp/entities/Funcionario.java
+++ src/main/java/com/vespertino/hotelvesp/entities/Funcionario.java
@@ -3,6 +3,7 @@ package com.vespertino.hotelvesp.entities;
 
 import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 
 @Entity
@@ -10,21 +11,29 @@ import java.math.BigDecimal;
 public class Funcionario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    @Column(name="id", nullable = false, unique = true, length = 10)
+    @Pattern(regexp="[A-z0-9\s]{1,10}")
     private Integer id;
-    @Column(name="nome")
+    @Column(name="nome", nullable = false, length = 255)
+    @Pattern(regexp="[A-z\s]{1,255}")
     private String nome;
-    @Column(name="email")
+    @Column(name="email", nullable = false, unique = true, length = 255)
+    @Pattern(regexp ="[A-z]{1,64}@[A-Za-z0-9]{1,255}.[A-z]{1,3}" )
     private String email;
-    @Column(name="telefone")
+    @Column(name="telefone", nullable = false, unique = true, length = 255)
+    @Pattern(regexp = "[0-9]{2}[\\s][0-9]{5}-[0-9]{4}")
     private String telefone;
-    @Column(name="salario")
+    @Column(name="salario", nullable = false, length = 15)
+    @Pattern(regexp = "[0-9]{1,15}[.][0-9]{1,2}")
     private BigDecimal salario;
-    @Column(name="cargo")
+    @Column(name="cargo", nullable = false, length = 255)
+    @Pattern(regexp = "[A-z\s]{1,255}]")
     private String cargo;
-    @Column(name="ativo")
+    @Column(name="ativo", nullable = false)
+    @Pattern(regexp = "(?i)(true|false)")
     private Boolean ativo;
-    @Column(name="id_lancamento")
+    @Column(name="id_lancamento", nullable = false, unique = true, length = 10)
+    @Pattern(regexp = "[0-9]{10}")
     private Integer idLancamento;
 
     public Integer getId() {
