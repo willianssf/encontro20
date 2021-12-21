e7f38852e118e0a3c36ee4576c0665bae13d2e79
Pedrinho-o-brabo <preitman@itelinfo.com>
2021-12-21 15:21:22 -0400

    add andar repo

--- src/main/java/com/vespertino/hotelvesp/repositories/PratoRepository.java
+++ src/main/java/com/vespertino/hotelvesp/repositories/PratoRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface PratoRepository extends JpaRepository<Prato, Integer> {
 
     List<Prato> findByAtivo(Boolean ativo);
+    List<Prato> findByNome(String nome);
 }
