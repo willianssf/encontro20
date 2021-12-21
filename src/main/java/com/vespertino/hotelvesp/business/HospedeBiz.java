eb838c81d38e1b344fd0b515817b8b6cbf6b7cfc
enzookita <eokita@itelinfo.com>
2021-12-21 16:50:38 -0400

    BizHospede

--- src/main/java/com/vespertino/hotelvesp/business/HospedeBiz.java
+++ src/main/java/com/vespertino/hotelvesp/business/HospedeBiz.java
@@ -1,15 +1,16 @@
 package com.vespertino.hotelvesp.business;
 
-import com.vespertino.hotelvesp.entities.Funcionario;
 import com.vespertino.hotelvesp.entities.Hospede;
-import com.vespertino.hotelvesp.repositories.FuncionarioRepository;
+import com.vespertino.hotelvesp.entities.Quarto;
 import com.vespertino.hotelvesp.repositories.HospedeRepository;
+import com.vespertino.hotelvesp.repositories.QuartoRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HospedeBiz {
 
+    private QuartoRepository quartoRepository;
     private Hospede hospede;
     private HospedeRepository hospedeRepository;
     private List<String> erros;
@@ -30,6 +31,11 @@ public class HospedeBiz {
 
     public Boolean isValid(){
         Boolean resultado = telefone11Numero(this.hospede.getTelefone());
+        resultado = nomePreenchido(this.hospede.getNome()) && resultado;
+        resultado = nomeAtendeCaracteres(this.hospede.getNome()) && resultado;
+        resultado = emailPreenchido(this.hospede.getEmail()) && resultado;
+        resultado = limiteCredito0a1000000(this.hospede.getLimite_credito()) && resultado;
+
         return resultado;
     }
 
@@ -41,4 +47,42 @@ public class HospedeBiz {
         return resultado;
 
     }
+
+    public Boolean nomePreenchido (String nome){
+        Boolean resultado = nome.matches("^[A-z\s]{1,}$");
+        if (!resultado){
+            erros.add("O nome nao pode estar vazio");
+        }
+        return resultado;
+    }
+    public Boolean nomeAtendeCaracteres (String nome){
+        Boolean resultado = nome.matches("^[A-z\s]{10,50}$");
+        if (!resultado){
+            erros.add("O nome deve ter entre 10 e 50 caracteres");
+        }
+        return resultado;
+    }
+    public Boolean emailPreenchido(String email){
+        Boolean resultado = !email.isEmpty();
+        if (!resultado){
+            erros.add("O email nao pode estar vazio");
+        }
+        return resultado;
+    }
+    public Boolean limiteCredito0a1000000 (Double limiteCredito){
+        Boolean resultado = limiteCredito.doubleValue() < 1000000 && limiteCredito.doubleValue() > 0;
+        if (!resultado){
+            erros.add("O limite de credito deve estar acima de zero e abaixo de 1000000!");
+        }
+        return resultado;
+    }
+    //public Boolean quartoExiste( Integer idquarto ) {
+       // Quarto lista = quartoRepository.findById(idquarto).stream();
+       // Boolean resultado = !lista.equals();
+        //if (!resultado) {
+         //   erros.add("Quarto não existe!");
+       // }
+       // return resultado;
+    //}
+
 }
