6003328f9d028385e633879e77b13ceab8721290
narciza-fialho <mfialho@itelinfo.com>
2021-12-21 15:37:31 -0400

    metodos PratoBiz

--- src/main/java/com/vespertino/hotelvesp/business/PratoBiz.java
+++ src/main/java/com/vespertino/hotelvesp/business/PratoBiz.java
@@ -26,7 +26,9 @@ public class PratoBiz {
     }
 
     public Boolean isValid() {
-        Boolean resultado = true;
+        Boolean resultado = verificadorNome(this.prato.getNome());
+        resultado = nomeNaoExiste(this.prato.getNome()) && resultado;
+        resultado = valorAcima0(this.prato.getValor()) && resultado;
         return resultado;
     }
 
@@ -34,5 +36,28 @@ public class PratoBiz {
     Não pode ter nome repetido no banco de dados
     O valor deve ser acima de 0*/
 
+    public Boolean verificadorNome (String nome) {
+        Boolean resultado = nome.matches("[A-Za-z]{5,50}");
+        if (!resultado) {
+            erros.add("Nome menor que 5 ou maior que 50");
+        }
+        return resultado;
+    }
 
+    public Boolean nomeNaoExiste (String nome) {
+        List<Prato> lista = pratoRepository.findByNome(nome);
+        Boolean resultado = lista.isEmpty();
+        if (!resultado) {
+            erros.add("Este nome ja existe");
+        }
+        return resultado;
+    }
+
+    public Boolean valorAcima0 (Double valor) {
+        Boolean resultado = valor > 0;
+        if (!resultado) {
+            erros.add("Valor não pode ser 0");
+        }
+        return resultado;
+    }
 }
